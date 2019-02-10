7ef1aa555685ea9fb1bf34d7414515b532794319
dcrayne <dan@crayne.net>
2019-02-10 14:30:43 -0800

    Initial commit

---
+++ src/App/Program.fs
@@ -0,0 +1,12 @@
+﻿open Library
+
+[<EntryPoint>]
+let main argv =
+    printfn "Nice command-line arguments! Here's what Json.NET has to say about them:"
+
+    // argv
+    // |> Array.map getJsonNetJson
+    // |> Array.iter (printfn "%s")
+    printfn "%s" (getPeople "Data/People.json")
+
+    0
---
+++ src/Library/Library.fs
@@ -0,0 +1,15 @@
+﻿module Library
+
+open Newtonsoft.Json
+open System.IO
+
+let getJsonNetJson value =
+    sprintf "I used to be %s but now I'm %s thanks to Json.NET!" value (JsonConvert.SerializeObject(value))
+
+type Person = {
+    name: string;
+    email: string
+}
+
+let getPeople filePath =
+    File.ReadAllText(filePath)
