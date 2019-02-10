0d51a04e92f09f861da340274a942a017cf64f37
dcrayne <dan@crayne.net>
2019-02-10 14:38:49 -0800

    Get rid of template stuff

--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -3,9 +3,6 @@
 open Newtonsoft.Json
 open System.IO
 
-let getJsonNetJson value =
-    sprintf "I used to be %s but now I'm %s thanks to Json.NET!" value (JsonConvert.SerializeObject(value))
-
 type Person = {
     name: string;
     email: string
